69b6b0cd08b26e2977d9b54f462f9b22869d2d51
Olivia Solorzano <omsolorz@ucsd.edu>
2023-05-25 11:41:04 -0700

    Class 6

---
+++ Class06/Class06.R
@@ -0,0 +1,6 @@
+student1 <- c(100, 100, 100, 100, 100, 100, 100, 90)
+student2 <- c(100, NA, 90, 90, 90, 90, 97, 80)
+student3 <- c(90, NA, NA, NA, NA, NA, NA, NA)
+
+#We can use the `mean()` function to calculate the average for a given studentvector.
+
